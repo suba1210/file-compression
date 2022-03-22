d1953899cd324f367dcb69ed644ea59a6c1036dd
suba1210 <subasris1210@gmail.com>
2022-03-22 19:22:14 +0530

    feat: algorithms

--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
+const Huffman = require('../algorithms/huffman/huffman');
+const Lzw = require('../algorithms/lzw/lzw');
+const Lz77 = require("lzbase62");
 
 //file upload
 const storage = multer.diskStorage({
@@ -19,6 +22,17 @@ const upload = multer({
         checkFileType(file, callback);
     }
 });
+
+const readWriteFile = (fileName, encodedString, type) => {
+    fileName = fileName.slice(0,fileName.length-3);
+    let writeStream = fs.createWriteStream(`./public/uploads/${fileName}${type}`);
+    writeStream.write(encodedString);  
+    let stats = fs.statSync(`./public/uploads/${fileName}${type}`) ;
+    console.log(stats);
+    // let fileSizeInBytes = stats.size;
+    let fileSizeInBytes = 0;
+    return fileSizeInBytes;
+}
 
 function checkFileType(file, callback, req, res) {
     const filetypes = [
@@ -39,7 +53,21 @@ router.get("/home", async (req, res) => {
 
 
 router.post("/addFile", upload.single("file"), async (req, res) => {
-    res.send(req.file.filename);
+    const data = fs.readFileSync(`./public/uploads/${req.file.filename}`,{encoding:'utf8', flag:'r'});
+
+    const huffmanObj = new Huffman();
+    let [encodedHuffmanString, outputMsg] = huffmanObj.encode(data);
+    let huffSize = readWriteFile(req.file.filename, encodedHuffmanString, 'huff');
+
+    const lzwObj = new Lzw();
+    let encodedLzwString = lzwObj.encode(data);
+    let lzwSize = readWriteFile(req.file.filename,encodedLzwString,"lzw");
+
+    let encodedLz77String = Lz77.compress(data);
+    let lz77Size = readWriteFile(req.file.filename,encodedLz77String,"lz77");
+
+    res.send(`huffSize = ${huffSize}, lzwSize = ${lzwSize}, lz77Size = ${lz77Size}`);
+
 });
 
 module.exports = router;
