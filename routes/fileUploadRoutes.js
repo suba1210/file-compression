c3ca1d1c0195e8ed653f8e590dbbc708fc17e24b
suba1210 <subasris1210@gmail.com>
2022-03-22 17:14:23 +0530

    feat: multer setup

--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -1,53 +1,45 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const fs = require('fs');
-const multer = require('multer');
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
 
+//file upload
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename : function(req, file, cb){
-        cb(null,file.fieldname + '-' +Date.now()+path.extname(file.originalname));
+    destination: './public/uploads',
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now() + 
+        path.extname(file.originalname));
     }
 });
-
 
 const upload = multer({
     storage: storage,
-    fileFilter : function(req,file,cb){
-        checkFileType(file,cb);
+    fileFilter: function(req, file, callback) {
+        checkFileType(file, callback);
     }
-}).single('image');
+});
 
+function checkFileType(file, callback, req, res) {
+    const filetypes = [
+        "text/plain"
+    ];
+    const mimetype = filetypes.includes(file.mimetype);
 
-function checkFileType(file,cb){
-    const filetypes = /txt|jpg|png|gif/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if(mimetype && extname){
-        return cb(null,true);
-    }else {
-        cb('Error : Images only');
+    if(mimetype) {
+        return callback(null, true);
+    } else {
+        callback("Error: Images only!");
     }
 }
 
-router.get('/home' , async(req,res)=>{
-    res.render('homePage');
-});
-
-router.post('/addFile', async(req,res)=>{
-    upload(req,res,(err) => {
-        if(err){
-            res.send('error');
-            console.log(err);
-        }  else {
-            let uploadedFile = fs.readFileSync(path.join('./public/uploads/' + req.file.filename));  
-            res.send(uploadedFile);
-        }
-    });
+router.get("/home", async (req, res) => {
+  res.render("homePage");
 });
 
 
+router.post("/addFile", upload.single("file"), async (req, res) => {
+    res.send(req.file.filename);
+});
 
 module.exports = router;
