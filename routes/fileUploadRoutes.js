9dd5d6a9edf3781e949af407af7effb83015092a
suba1210 <subasris1210@gmail.com>
2022-03-23 18:03:55 +0530

    feat: file size, finally made statSync work :)

--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -6,6 +6,8 @@ const multer = require("multer");
 const Huffman = require("../algorithms/huffman/huffman");
 const Lzw = require("../algorithms/lzw/lzw");
 const Lz77 = require("lzbase62");
+let huffSize,lzwSize,lz77Size;
+
 
 //file upload
 const storage = multer.diskStorage({
@@ -25,13 +27,29 @@ const upload = multer({
   },
 });
 
-const readWriteFile = (fileName, encodedString, type) => {
-  fileName = fileName.slice(0, fileName.length - 4);
-  let writeStream = fs.createWriteStream(
-    `./public/uploads/${fileName}.${type}`
-  );
-  writeStream.write(encodedString);
-  return;
+const waitAndGetSize = async(fileName1,type) => {
+    let sizeInBytes;
+    return await new Promise(resolve => {
+      const interval = setInterval(() => {
+        if (fs.existsSync(fileName1) && fs.statSync(fileName1).size !== 0) {
+          clearInterval(interval);
+          sizeInBytes = fs.statSync(fileName1).size;
+          resolve(sizeInBytes);
+        };
+      }, 1000);
+    });
+    
+}
+
+const readWriteFile = async(fileName, encodedString, type) => {
+  return await new Promise(resolve => {
+    let filePath = `./public/uploads/${fileName.slice(0, fileName.length - 4)}.${type}`;
+    let writeStream = fs.createWriteStream(filePath);
+    writeStream.write(encodedString);
+    let size = waitAndGetSize(filePath, type);
+    resolve(size);
+  })
+
 };
 
 function checkFileType(file, callback, req, res) {
@@ -57,74 +75,17 @@ router.post("/addFile", upload.single("file"), async (req, res) => {
 
   const huffmanObj = new Huffman();
   let [encodedHuffmanString, outputMsg] = huffmanObj.encode(data);
-  readWriteFile(req.file.filename, encodedHuffmanString, "huff");
-  let fileName1 = req.file.filename;
-  fileName1 =
-    "./public/uploads/" + fileName1.slice(0, fileName1.length - 4) + ".huff";
-  console.log(fileName1);
-
-  let counter = 0;
-  let threshold = 100;
-
-  let interval = setInterval(() => {
-    if (fs.existsSync(fileName1) && fs.statSync(fileName1).size !== 0) {
-      clearInterval(interval);
-      stats = fs.statSync(fileName1);
-      console.log("huffStats", stats);
-    } else if (counter <= threshold) {
-      counter++;
-    } else {
-      clearInterval(interval);
-      reject(new Error(`${fileName1} was not created.`));
-    }
-  }, 1000);
+  huffSize = await readWriteFile(req.file.filename, encodedHuffmanString, "huff");
 
   const lzwObj = new Lzw();
   let encodedLzwString = lzwObj.encode(data);
-  readWriteFile(req.file.filename, encodedLzwString, "lzw");
-  let fileName2 = req.file.filename;
-  fileName2 =
-    "./public/uploads/" + fileName2.slice(0, fileName2.length - 4) + ".lzw";
-
-  counter = 0;
-  threshold = 100;
-
-  interval = setInterval(() => {
-    if (fs.existsSync(fileName2) && fs.statSync(fileName2).size !== 0) {
-      clearInterval(interval);
-      stats = fs.statSync(fileName2);
-      console.log("lzwStats", stats);
-    } else if (counter <= threshold) {
-      counter++;
-    } else {
-      clearInterval(interval);
-      reject(new Error(`${fileName2} was not created.`));
-    }
-  }, 1000);
+  lzwSize = await readWriteFile(req.file.filename, encodedLzwString, "lzw");
 
   let encodedLz77String = Lz77.compress(data);
-  readWriteFile(req.file.filename, encodedLz77String, "lz77");
-  let fileName3 = req.file.filename;
-  fileName3 =
-    "./public/uploads/" + fileName2.slice(0, fileName2.length - 4) + ".lz77";
+  lz77Size = await readWriteFile(req.file.filename, encodedLz77String, "lz77");
 
-  counter = 0;
-  threshold = 100;
+  res.send('huff size: ' + huffSize + ' lzw size: ' + lzwSize + ' lz77 size: ' + lz77Size);
 
-  interval = setInterval(() => {
-    if (fs.existsSync(fileName3) && fs.statSync(fileName3).size !== 0) {
-      clearInterval(interval);
-      stats = fs.statSync(fileName3);
-      console.log("lz77Stats", stats);
-    } else if (counter <= threshold) {
-      counter++;
-    } else {
-      clearInterval(interval);
-      reject(new Error(`${fileName3} was not created.`));
-    }
-  }, 1000);
-
-  res.send(`summa`);
 });
 
 module.exports = router;
